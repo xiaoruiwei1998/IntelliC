7e8023f7a42dc696b8352522513614e6fbbaa1ec
Ruiwei Xiao <ruiweixiao@berkeley.edu>
2020-03-22 02:57:58 +0800

    CRUD

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var model = require('../model');
 router.get('/', function(req, res, next) {
   model.connect(function(db) {
     db.collection('users').find().toArray(function(err, docs) {
-      console.log('user docs', docs)
+      // console.log('user docs', docs)
       res.render('index', { title: 'Express' });
     })
   })
@@ -37,15 +37,51 @@ router.get('/logPage', function(req, res, next) {
   res.render('logPage', {})
 })
 
-/* GET category page. */
-router.get('/categoryPage', function(req, res, next) {
-    res.render('categoryPage', {})
+/* GET course page. */
+router.get('/coursePage', function(req, res, next) {
+    res.render('coursePage', {})
   })
 
 /* GET all questions page. */
 router.get('/allQuestionsPage', function(req, res, next) {
   var userMail = req.session.userMail || ''
-  res.render('allQuestionsPage', {userMail: userMail})
+  var page = req.query.page || 1
+  var data = {
+    total: 0, // total page number
+    curPage: page,
+    list: []  // all questions in curPage
+  }
+  var pageSize = 2
+  model.connect(function(db) {
+    db.collection('questions').find().toArray(function(err, docs) {
+        console.log('allQuestions', err)
+        var list = docs
+        res.render('allQuestionsPage', {userMail: userMail, list: list, data: data});
+    })
+})
+})
+
+/* GET edit Question page. */
+router.get('/editQuestionPage', function(req, res, next) {
+  var userMail = req.session.userMail || ''
+  var item = {
+    q_title: '',
+    q_type: '',
+    q_description: '',
+    q_answer: '',
+    q_explaination: '',
+    q_time: ''
+}
+  if (req.query.q_description) {
+    console.log(req.query.q_description)
+    model.connect(function(db) {
+      db.collection('questions').findOne({q_type: req.query.q_type, q_description: req.query.q_description}, function(err, docs) {
+          item = docs
+          res.render('editQuestionPage', {userMail: userMail, item: item})
+      })
+  })} else {
+    res.render('editQuestionPage', {userMail: userMail, item: item});
+  }
 })
 
 /* GET testPaper page. */
--- routes/questions.js
+++ routes/questions.js
@@ -1,32 +1,81 @@
 var express = require('express');
 var router = express.Router();
 var model = require('../model');
-
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
-  });
+var moment = require('moment');
 
 // INSERT QUESTION
-router.post('/insert', function(req, res, next) {
-    console.log('发布问题')
+router.post("/insert", function(req, res, next) {
+    // if high similiarity question already exists, ...
     var data = {
-        date: Date.now(),
-        title: req.body.title,
-        type: req.body.type,
-        description: req.body.description,
-        answer: req.body.answer,
-        explaination: req.body.explaination
+        q_title: req.body.title,
+        q_type: req.body.type,
+        q_description: req.body.description,
+        q_answer: req.body.answer,
+        q_explaination: req.body.explaination,
+        q_time: moment(Date.now()).format('YYYY-MM-DD HH:mm:ss')
     }
+    if (req.body.q_time) {
+        model.connect(function(db) {
+            db.collection('questions').updateOne({q_time: req.body.q_time}, {$set: data}, function(err, ret) {
+                if (err) {
+                    console.log('update err!')
+                } else {
+                    console.log('updated!')
+                }
+            })
+        })
+    } else {
+        model.connect(function(db) {
+            db.collection('questions').insertOne(data, function(err, ret) {
+                if (err) {
+                    console.log('添加问题失败！', err)
+                    res.redirect('/allQuestionsPage')
+                } else {
+                    res.redirect('/testPaperPage')
+                }
+            })
+        })
+    }
+});
+
+// EDIT QUESTION
+router.get("/edit", function(req, res, next) {
+    var data = {
+        q_title: '',
+        q_type: '',
+        q_description: '',
+        q_answer: '',
+        q_explaination: '',
+        q_time: ''
+    }
+    if (req.query.q_time) {
+        model.connect(function(db) {
+            db.collection('questions').findOne({q_time: req.query.q_time}, function(err, docs) {
+                if (err) {
+                    console.log('Edit failed!')
+                } else {
+                    data = docs
+                    res.render('edit', {data: data})
+                    console.log('Edit successfully')
+                }
+            })
+        })
+    } else {
+        res.render('edit', {data: data})
+    }
+    
+});
+
+// DELETE QUESTION
+router.get("/delete", function(req, res, next) {
     model.connect(function(db) {
-        console.log('连接数据库！')
-        db.collection('questions').insertOne(data, function(err, ret) {
+        db.collection('questions').deleteOne({q_time: req.query.q_time}, function(err, ret) {
             if (err) {
-                console.log('添加问题失败！', err)
-                res.redirect('/allQuestionsPage')
+                console.log('Delete failed!')
             } else {
-                console.log('添加问题成功！')
-                res.redirect('/testPaperPage')
+                console.log('Delete successfully')
             }
+            res.redirect('/allQuestionsPage')
         })
     })
 });
